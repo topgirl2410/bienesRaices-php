b10a83f06712427bd820a29b99ba153bd1f8c121
topgirl2410 <maria.topgirl82@gmail.com>
2022-12-08 22:35:58 +0100

    agregando la fecha con php

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -8,7 +8,9 @@
         </nav>
     </div>
 
-    <p class="copyright">Todos los derechos Reservados 2021 &copy;</p>
+
+
+    <p class="copyright">Todos los derechos Reservados <?php echo date('Y') ?> &copy;</p>
 </footer>
 
 <script src="build/js/bundle.min.js"></script>
