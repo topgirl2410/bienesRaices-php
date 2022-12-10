6da713c68cbd63955362c0434a212d62b58e0322
topgirl2410 <maria.topgirl82@gmail.com>
2022-12-10 15:51:30 +0100

    llenar los campos con un valor previo

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -6,6 +6,16 @@ $db = conectarDB();
 // Arreglo con mensajes de errores
 $errores = [];
 
+$titulo = '';
+$precio = '';
+$descripcion = '';
+$habitaciones = '';
+$wc = '';
+$parking = '';
+$vendedores_id = '';
+
+
+
 // Ejecutar el código después de que el usuario envia el formulario
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
@@ -63,11 +73,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-
-
-
-
-
 require '../../includes/funciones.php';
 incluirTemplate('header');
 ?>
@@ -90,16 +95,16 @@ incluirTemplate('header');
             </legend>
 
             <label for="titulo">Titulo:</label>
-            <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad">
+            <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad" value="<?php echo $titulo; ?>">
 
             <label for="precio">Precio:</label>
-            <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
+            <input type="number" id="precio" name="precio" placeholder="Precio Propiedad"value="<?php echo $precio; ?>" >
 
             <label for="imagen">Imagen:</label>
             <input type="file" id="imagen" accept="image/jpeg, image/png">
 
             <label for="descripcion">Descripción:</label>
-            <textarea id="descripcion" name="descripcion"></textarea>
+            <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
 
         </fieldset>
 
@@ -107,19 +112,19 @@ incluirTemplate('header');
             <legend>Información de la Propiedad</legend>
 
             <label for="habitaciones">Habitaciones:</label>
-            <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
+            <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9" value="<?php echo $habitaciones; ?>">
 
             <label for="wc">Baños:</label>
-            <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9">
+            <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9" value="<?php echo $wc; ?>">
 
             <label for="parking">Parking:</label>
-            <input type="number" id="parking" name="parking" placeholder="Ej: 3" min="1" max="9">
+            <input type="number" id="parking" name="parking" placeholder="Ej: 3" min="1" max="9" value="<?php echo $parking; ?>">
         </fieldset>
 
         <fieldset>
             <legend>Vendedor</legend>
             <select name="vendedores_id">
-                <option disabled selected value="">-- Selecciona Vendedor --</option>
+                <option disabled selected value="<?php echo $vendedores_id; ?>">-- Selecciona Vendedor --</option>
                 <option value="1">María</option>
                 <option value="2">Marco</option>
             </select>
