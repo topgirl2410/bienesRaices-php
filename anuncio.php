cb491ef7bdb1d0bf4b7bd4a908300e33b01786ef
topgirl2410 <maria.topgirl82@gmail.com>
2022-12-12 23:38:36 +0100

    CRUD acabado

--- anuncio.php
+++ anuncio.php
@@ -1,46 +1,63 @@
 <?php
+$id = $_GET['id'];
+$id = filter_var($id, FILTER_VALIDATE_INT);
+
+if (!$id) {
+    header('Location: /');
+}
+
+// Importar la conexión
+require 'includes/config/database.php';
+$db = conectarDB();
+
+// Consultar la BD 
+$query = "SELECT * FROM propiedades WHERE id = ${id}";
+
+// Obtener los resultados
+$resultado = mysqli_query($db, $query);
+
+if (!$resultado->num_rows) {
+    header('Location: /');
+}
+
+$propiedad = mysqli_fetch_assoc($resultado);
+
+
 require 'includes/funciones.php';
 incluirTemplate('header');
 ?>
 
 <main class="contenedor seccion contenido-centrado">
-    <h1>Casa en Venta Frente al Bosque</h1>
-    <picture>
-        <source srcset="build/img/destacada.webp" type="image/webp">
-        <source srcset="build/img/destacada.jpg" type="image/jpeg">
-        <img loading="lazy" src="build/img/destacada.jpg" alt="imagen de la propiedad">
-    </picture>
+    <h1><?php echo $propiedad['titulo']; ?></h1>
+
+    <img loading="lazy" src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="imagen de la propiedad">
 
     <div class="resumen-propiedad">
-        <p class="precio">3.000.000 €</p>
+        <p class="precio"><?php echo $propiedad['precio']; ?> €</p>
 
         <ul class="iconos-caracteristicas">
             <li>
                 <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                <p>2</p>
+                <p><?php echo $propiedad['wc']; ?></p>
             </li>
             <li>
                 <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono dormitorio">
-                <p>3</p>
+                <p><?php echo $propiedad['habitaciones']; ?></p>
             </li>
             <li>
                 <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                <p>3</p>
+                <p><?php echo $propiedad['parking']; ?></p>
             </li>
         </ul>
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when
-            looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution
-            of letters, as opposed to using 'Content here, content here', making it look like readable English. Many
-            desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
-            search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
-            evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-            into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-            software like Aldus PageMaker including versions of Lorem Ipsum</p>
+        <p><?php echo $propiedad['descripcion']; ?></p>
+        <p><?php echo $propiedad['descripcion']; ?></p>
     </div>
 </main>
 
-<?php incluirTemplate('footer') ?>
+<?php
+
+// Cerrar conexión
+mysqli_close($db);
+
+incluirTemplate('footer')
+?>
