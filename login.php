1bac7247f088dfb0c1512d09485acfa277ef15c9
topgirl2410 <maria.topgirl82@gmail.com>
2022-12-16 00:42:58 +0100

    validacion en el formulario

--- login.php
+++ login.php
@@ -1,11 +1,49 @@
 <?php
+
+// Conectar base de datos
+require 'includes/config/database.php';
+$db = conectarDB();
+
+// Autentificar un usuario
+$errores = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
+
+    $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+    var_dump($email);
+
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if (!$email) {
+        $errores[] = "El email es obligatorio o no es válido";
+    }
+
+    if (!$password) {
+        $errores[] = "El password es obligatorio";
+    }
+}
+
+
+// Incluye el header
 require 'includes/funciones.php';
 incluirTemplate('header');
 ?>
 
 <main class="contenedor seccion contenido-centrado">
     <h1>Iniciar Sesión</h1>
-    <form class="formulario">
+    <?php foreach ($errores as $error) : ?>
+        <div class="alerta error">
+            <?php echo $error ?>
+        </div>
+
+    <?php endforeach; ?>
+
+
+    <form method="POST" class="formulario">
         <fieldset>
             <legend>Email y Password</legend>
 
